3f24f9dedb963bb57ca6b52cc5c8fd4126f441e2
Esraa said <66054841+Esraa-Said@users.noreply.github.com>
2022-09-18 05:41:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// icon
+function myfunction(){
+    let cont = document.querySelector("#icon");
+    cont.classList.toggle("change");
+    let list = document.querySelector(".links_icon");
+    if(cont.className==="change")
+    list.style.display = "block";
+else     list.style.display = "none";
+
+    }
+
+// background
+
+
+
+
+
+
+
+
+
